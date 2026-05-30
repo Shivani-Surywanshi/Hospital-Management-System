3978950f08389aecffd1231e3c83aa08eb7fd0ea
Shivani Surywanshi <shivranishivrani736@gmail.com>
2026-05-30 23:44:44 +0530

    Main folder added

--- urls.py
+++ urls.py
@@ -1,7 +1,28 @@
-from django.urls import path
-from paymentapp import views
+"""
+URL configuration for project project.
+
+The `urlpatterns` list routes URLs to views. For more information please see:
+    https://docs.djangoproject.com/en/6.0/topics/http/urls/
+Examples:
+Function views
+    1. Add an import:  from my_app import views
+    2. Add a URL to urlpatterns:  path('', views.home, name='home')
+Class-based views
+    1. Add an import:  from other_app.views import Home
+    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
+Including another URLconf
+    1. Import the include() function: from django.urls import include, path
+    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
+"""
+from django.contrib import admin
+from django.urls import path,include
+from django.conf.urls.static import static
+from django.conf import settings
 
 urlpatterns = [
-    path('discharge/', views.discharge, name='discharge'),
-    path('final_bill/<int:id>/', views.final_bill, name='final_bill'),
-]
+    path('',include('userapp.urls')),
+    path('doctor/', include('doctorapp.urls')),
+    path('report/', include('reportapp.urls')),
+    path('payment/', include('paymentapp.urls')),
+    path('admin/', admin.site.urls),
+] + static(settings.MEDIA_URL,document_root=settings.MEDIA_ROOT)
